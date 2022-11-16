b7cf431438bbf849e05090023480bde47139407f
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-11-16 11:54:15 +0700

    Creating Jump method for Cube

--- CubeRunner/Assets/Script/Cube.cs
+++ CubeRunner/Assets/Script/Cube.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    public int heightOfJump = 480;
+    Rigidbody2D _rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool isKeyJumpPressed = Input.GetKeyDown(KeyCode.Space);
+
+        if (isKeyJumpPressed)
+        {
+            _rb.AddForce(Vector2.up * new Vector2(0, heightOfJump));
+        }
     }
 }
